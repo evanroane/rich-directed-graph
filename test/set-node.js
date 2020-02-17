40e86f87dfc20ed2b3a6a46264d217caeecd6c1f
Evan Roane <evan.roane@gmail.com>
2020-02-17 13:36:07 -0600

    more setNode tests

--- test/set-node.js
+++ test/set-node.js
@@ -12,5 +12,20 @@ describe('Directed Graph', () => {
 
       assert.equal(graph.nodes.get('desire').def, 'appetite together with consciousness of the appetite');
     });
+
+    it('should add a second, independent node', () => {
+      graph.setNode('joy', {
+        def: 'Joy is man\'s passage from a less to a greater perfection'
+      });
+
+      assert.equal(graph.nodes.get('desire').def, 'appetite together with consciousness of the appetite');
+      assert.equal(graph.nodes.get('joy').def, 'Joy is man\'s passage from a less to a greater perfection');
+    });
+
+    it('should replace the node when set on the same key', () => {
+      graph.setNode('desire', { def: 'something else' });
+
+      assert.equal(graph.nodes.get('desire').def, 'something else');
+    });
   });
 });
