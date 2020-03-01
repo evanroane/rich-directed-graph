29c64fdeb8014b64e401a56ef113822c6e12c79d
Evan Roane <evan.roane@gmail.com>
2020-03-01 13:11:18 -0600

    clean up setNode tests and tansition to expect syntax

--- test/set-node.ts
+++ test/set-node.ts
@@ -1,15 +1,21 @@
-import { assert } from 'chai';
+import { expect } from 'chai';
 import DirectedGraph from '../src/index';
+import { describe, it } from 'mocha';
 
 describe('setNode', () => {
   const graph = new DirectedGraph();
+
+  it('should set a node with an empty object if no content is provided', () => {
+    graph.setNode('');
+    expect(graph.nodes.size).to.equal(1);
+  });
 
   it('should add a node', () => {
     graph.setNode('desire', {
       def: 'appetite together with consciousness of the appetite'
     });
 
-    assert.equal(graph.nodes.get('desire').def, 'appetite together with consciousness of the appetite');
+    expect(graph.nodes.get('desire').def).to.equal('appetite together with consciousness of the appetite');
   });
 
   it('should add a second, independent node', () => {
@@ -17,13 +23,13 @@ describe('setNode', () => {
       def: 'passage from a lesser to a greater perfection'
     });
 
-    assert.equal(graph.nodes.get('desire').def, 'appetite together with consciousness of the appetite');
-    assert.equal(graph.nodes.get('joy').def, 'passage from a lesser to a greater perfection');
+    expect(graph.nodes.get('desire').def).to.equal('appetite together with consciousness of the appetite');
+    expect(graph.nodes.get('joy').def).to.equal('passage from a lesser to a greater perfection');
   });
 
   it('should replace the node when set on the same key', () => {
     graph.setNode('desire', { def: 'something else' });
 
-    assert.equal(graph.nodes.get('desire').def, 'something else');
+    expect(graph.nodes.get('desire').def).to.equal('something else');
   });
-})
+});
