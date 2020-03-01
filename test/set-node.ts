773c0d54753a28671a778398e766716af17823b7
Evan Roane <evan.roane@gmail.com>
2020-03-01 12:52:09 -0600

    clean up setNode test

--- test/set-node.ts
+++ test/set-node.ts
@@ -1,31 +1,29 @@
 import { assert } from 'chai';
 import DirectedGraph from '../src/index';
 
-describe('Directed Graph', () => {
+describe('setNode', () => {
   const graph = new DirectedGraph();
 
-  describe('setNode', () => {
-    it('should add a node', () => {
-      graph.setNode('desire', {
-        def: 'appetite together with consciousness of the appetite'
-      });
-
-      assert.equal(graph.nodes.get('desire').def, 'appetite together with consciousness of the appetite');
+  it('should add a node', () => {
+    graph.setNode('desire', {
+      def: 'appetite together with consciousness of the appetite'
     });
 
-    it('should add a second, independent node', () => {
-      graph.setNode('joy', {
-        def: 'Joy is man\'s passage from a less to a greater perfection'
-      });
-
-      assert.equal(graph.nodes.get('desire').def, 'appetite together with consciousness of the appetite');
-      assert.equal(graph.nodes.get('joy').def, 'Joy is man\'s passage from a less to a greater perfection');
-    });
-
-    it('should replace the node when set on the same key', () => {
-      graph.setNode('desire', { def: 'something else' });
-
-      assert.equal(graph.nodes.get('desire').def, 'something else');
-    });
+    assert.equal(graph.nodes.get('desire').def, 'appetite together with consciousness of the appetite');
   });
-});
+
+  it('should add a second, independent node', () => {
+    graph.setNode('joy', {
+      def: 'passage from a lesser to a greater perfection'
+    });
+
+    assert.equal(graph.nodes.get('desire').def, 'appetite together with consciousness of the appetite');
+    assert.equal(graph.nodes.get('joy').def, 'passage from a lesser to a greater perfection');
+  });
+
+  it('should replace the node when set on the same key', () => {
+    graph.setNode('desire', { def: 'something else' });
+
+    assert.equal(graph.nodes.get('desire').def, 'something else');
+  });
+})
