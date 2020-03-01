41944361c0bd5a6634a66f02a911f67a77b089a1
Evan Roane <evan.roane@gmail.com>
2020-03-01 14:56:49 -0600

    test delete node

---
+++ test/delete-node.ts
@@ -0,0 +1,22 @@
+import populatedGraph from './populated-graph';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+describe('deleteEdge', () => {
+  it('should do something', () => {
+    const graph = populatedGraph;
+
+    //assert where hate is
+    expect(graph.edges.has('hate')).to.be.true;
+    expect(graph.nodes.has('hate')).to.be.true;
+    expect(graph.edges.get('mockery')).to.include('hate');
+
+    // remove hate
+    graph.deleteNode('hate');
+
+    // assert where hate is not
+    expect(graph.edges.has('hate')).to.be.false;
+    expect(graph.nodes.has('hate')).to.be.false;
+    expect(graph.edges.get('mockery')).to.not.include('hate');
+  });
+});
